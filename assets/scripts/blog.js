59cbc97bb1c2bc07cdeb57ee9f82b5d15b238e9f
Steward Lumowa <stewardyohanes29@gmail.com>
2024-04-15 19:49:45 +0800

    commit again

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -1,14 +1,20 @@
-const dataProject = [];
+const dataProject = JSON.parse(sessionStorage.getItem("dataProject")) || [];
 
 const addProject = (event) => {
   event.preventDefault();
 
   let projectName = document.getElementById("project-name").value;
+  !projectName && alert("Please Enter Your Project Name");
+
   let startDate = document.getElementById("start_date").value;
+  !startDate && alert("Please Enter Your Start Date");
+
   let endDate = document.getElementById("end_date").value;
+  !endDate && alert("Please Enter Your End Date");
+
   let description = document.getElementById("description").value;
-  let image = document.getElementById("file").files[0];
-  let imgSrc = URL.createObjectURL(image);
+  !description && alert("Please Enter Your Description");
+
   let technology = document.querySelectorAll("[type=checkbox]");
   let technologies = [];
   for (const tech of technology) {
@@ -16,6 +22,27 @@ const addProject = (event) => {
       technologies.push(tech.value);
     }
   }
+  technologies.length == 0 && alert("Please Select Your Technology");
+
+  let image = document.getElementById("file").files[0];
+  !image && alert("Please Upload Your Image");
+  let imgSrc = URL.createObjectURL(image);
+
+  const startDateFormat = new Date(startDate).toLocaleDateString("id-ID", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const endDateFormat = new Date(endDate).toLocaleDateString("id-ID", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  if (startDate > endDate) {
+    return alert("Start Date cannot be greater than End Date");
+  }
+
   const timeDiff = Date.parse(endDate) - Date.parse(startDate);
   const diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
   const diffMonths = Math.floor(diffDays / 30.4167);
@@ -32,21 +59,35 @@ const addProject = (event) => {
   }
 
   dataProject.push({
+    id: dataProject.length + 1,
+    startDateFormat,
+    endDateFormat,
     projectName,
     duration,
     description,
     technologies,
     imgSrc,
   });
-
+  sessionStorage.setItem("dataProject", JSON.stringify(dataProject));
   listProject();
 };
+
+window.addEventListener("DOMContentLoaded", () => {
+  listProject();
+});
 
 const listProject = () => {
   let projectList = document.querySelector(".card-project");
   projectList.innerHTML = "";
 
-  for (const project of dataProject) {
+  if (dataProject.length === 0) {
+    projectList.innerHTML = `<p style="font-style: italic; margin-bottom: 3rem">You don't have any project yet.</p>`;
+  }
+
+  dataProject.forEach((project) => {
+    const anchor = document.createElement("a");
+    anchor.href = `detail-project.html?id=${project.id}`;
+
     const card = document.createElement("div");
     card.classList.add("card");
 
@@ -55,7 +96,6 @@ const listProject = () => {
 
     const img = document.createElement("img");
     img.src = project.imgSrc;
-
     cardHeader.appendChild(img);
 
     const cardBody = document.createElement("div");
@@ -79,7 +119,6 @@ const listProject = () => {
       icon.classList.add(`fa-${tech}`);
       iconList.appendChild(icon);
     });
-
     cardBody.appendChild(h1);
     cardBody.appendChild(p);
     cardBody.appendChild(p2);
@@ -101,64 +140,7 @@ const listProject = () => {
     card.appendChild(cardBody);
     card.appendChild(cardFooter);
 
-    projectList.appendChild(card);
-  }
-
-  // dataProject.forEach((project) => {
-  //   const card = document.createElement("div");
-  //   card.classList.add("card");
-
-  //   const cardHeader = document.createElement("div");
-  //   cardHeader.classList.add("card-header");
-
-  //   const img = document.createElement("img");
-  //   img.src = project.imgSrc;
-
-  //   cardHeader.appendChild(img);
-
-  //   const cardBody = document.createElement("div");
-  //   cardBody.classList.add("card-body");
-
-  //   const h1 = document.createElement("h1");
-  //   h1.textContent = project.projectName;
-
-  //   const p = document.createElement("p");
-  //   p.textContent = `Duration : ${project.duration}`;
-
-  //   const p2 = document.createElement("p");
-  //   p2.textContent = project.description;
-
-  //   const iconList = document.createElement("div");
-  //   iconList.classList.add("icon");
-
-  //   project.technologies.forEach((tech) => {
-  //     const icon = document.createElement("i");
-  //     icon.classList.add("fa-brands");
-  //     icon.classList.add(`fa-${tech}`);
-  //     iconList.appendChild(icon);
-  //   });
-
-  //   cardBody.appendChild(h1);
-  //   cardBody.appendChild(p);
-  //   cardBody.appendChild(p2);
-  //   cardBody.appendChild(iconList);
-
-  //   const cardFooter = document.createElement("div");
-  //   cardFooter.classList.add("card-footer");
-
-  //   const buttonEdit = document.createElement("button");
-  //   buttonEdit.textContent = "Edit";
-
-  //   const buttonDelete = document.createElement("button");
-  //   buttonDelete.textContent = "Delete";
-
-  //   cardFooter.appendChild(buttonEdit);
-  //   cardFooter.appendChild(buttonDelete);
-
-  //   card.appendChild(cardHeader);
-  //   card.appendChild(cardBody);
-  //   card.appendChild(cardFooter);
-
-  //   projectList.appendChild(card);
-  // });
+    projectList.appendChild(anchor);
+    anchor.appendChild(card);
+  });
 };
